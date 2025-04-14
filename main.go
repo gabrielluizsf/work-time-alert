f7a22ca2d32ddbbe55172d2abb9520dcf0fef459
gabrielluizsf <93672097+gabrielluizsf@users.noreply.github.com>
2025-04-14 12:23:09 -0300

    refactor: Update nine library context import
    This commit updates the import path for the Context object from the "github.com/i9si-sistemas/nine" library to "github.com/i9si-sistemas/nine/pkg/server".

--- main.go
+++ main.go
@@ -4,12 +4,13 @@ import (
 	"log"
 
 	"github.com/i9si-sistemas/nine"
+	i9 "github.com/i9si-sistemas/nine/pkg/server"
 )
 
 func main() {
 	server := nine.NewServer("")
 	server.ServeFiles("/", "./public")
-	server.Use(func(c *nine.Context) error {
+	server.Use(func(c *i9.Context) error {
 		log.Printf("method=[%s] path=[%s] ips=%s", c.Method(), c.Path(), c.IPs())
 		return nil
 	})
