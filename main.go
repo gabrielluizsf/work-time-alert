9fac4b8a1703e42840e182b26f5c19c59e064ef5
gabrielluizsf <93672097+gabrielluizsf@users.noreply.github.com>
2025-04-17 08:48:26 -0300

    feat: using PORT environment variable

--- main.go
+++ main.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"github.com/i9si-sistemas/nine"
 	i9 "github.com/i9si-sistemas/nine/pkg/server"
 )
 
 func main() {
-	server := nine.NewServer("")
+	server := nine.NewServer(os.Getenv("PORT"))
 	server.ServeFiles("/", "./public")
 	server.Use(func(c *i9.Context) error {
 		log.Printf("method=[%s] path=[%s] ips=%s", c.Method(), c.Path(), c.IPs())
