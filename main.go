61b8b1f9ea62d34eba152378f16d53c5f288db04
gabrielluizsf <93672097+gabrielluizsf@users.noreply.github.com>
2025-04-17 11:26:44 -0300

    feat: implement session routes for webpush

--- main.go
+++ main.go
@@ -3,17 +3,24 @@ package main
 import (
 	"log"
 	"os"
+	"work-time-alert/server/logger"
+	"work-time-alert/server/session"
 
 	"github.com/i9si-sistemas/nine"
 	i9 "github.com/i9si-sistemas/nine/pkg/server"
 )
 
 func main() {
+	logger := logger.New()
 	server := nine.NewServer(os.Getenv("PORT"))
 	server.ServeFiles("/", "./public")
 	server.Use(func(c *i9.Context) error {
-		log.Printf("method=[%s] path=[%s] ips=%s", c.Method(), c.Path(), c.IPs())
+		logger.Request(c)
 		return nil
 	})
+	server.Get("/hello", func(c *i9.Context) error {
+		return c.Send([]byte("Hello World!"))
+	})
+	session.Routes(server)
 	log.Fatal(server.Listen())
 }
---
+++ server/session/routes.go
@@ -0,0 +1,80 @@
+package session
+
+import (
+	"crypto/rand"
+	"encoding/hex"
+	"net/http"
+	"work-time-alert/server/logger"
+	"work-time-alert/server/webpush"
+
+	"github.com/i9si-sistemas/nine"
+	i9 "github.com/i9si-sistemas/nine/pkg/server"
+)
+
+func Routes(server nine.Server) {
+	logger := logger.New()
+	
+	serviceWorker := server.Group("/notifier")
+
+	serviceWorker.Post("/session", func(c *i9.Context) error {
+		sessionBytes := make([]byte, 32)
+		_, err := rand.Read(sessionBytes)
+		if err != nil {
+			logger.Data("{status:500}", "{error:"+err.Error()+"}")
+			return c.SendStatus(http.StatusInternalServerError)
+		}
+		sessionId := hex.EncodeToString(sessionBytes)
+		Manager.Register(Subscription{
+			SenderId:     sessionId,
+			Subscription: Sub{},
+		})
+		return c.JSON(nine.JSON{"sessionId": sessionId})
+	})
+
+	serviceWorker.Get("/public", func(c *i9.Context) error {
+		key := webpush.Keys().PublicKey
+		return c.JSON(nine.JSON{
+			"key": key,
+		})
+	})
+
+	serviceWorker.Post("/subscribe", func(c *i9.Context) error {
+		var body struct {
+			SessionId string `json:"sessionId"`
+			Sub       Sub    `json:"subscription"`
+		}
+		if err := c.BodyParser(&body); err != nil {
+			logger.Data("{status:400}", "{error:"+err.Error()+"}")
+			return c.SendStatus(http.StatusBadRequest)
+		}
+		err := Manager.Update(body.SessionId, body.Sub)
+		if err != nil {
+			logger.Data("{status:404}", "{error:"+err.Error()+"}")
+			return c.SendStatus(http.StatusNotFound)
+		}
+		return c.SendStatus(http.StatusCreated)
+	})
+
+	serviceWorker.Post("/notify", func(c *i9.Context) error {
+		var body struct {
+			SessionId string `json:"sessionId"`
+			Title     string `json:"title"`
+			Body      string `json:"body"`
+		}
+		if err := c.BodyParser(&body); err != nil {
+			logger.Data("{status:400}", "{error:"+err.Error()+"}")
+			return c.SendStatus(http.StatusBadRequest)
+		}
+		err := webpush.Notify(body.SessionId, webpush.SubscriptionMessage{
+			SenderId: body.SessionId,
+			Title:    body.Title,
+			Body:     body.Body,
+		}, Manager)
+		if err != nil {
+			logger.Data("{status:500}", "{error:"+err.Error()+"}")
+			return c.SendStatus(http.StatusInternalServerError)
+		}
+		return c.SendStatus(http.StatusCreated)
+	})
+
+}
